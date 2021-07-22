745ecb0c365a0067990c62186e67c8811dbaa5cf
GMKYT <55257377+GMKYT@users.noreply.github.com>
2021-07-22 22:31:10 +0530

    Update private.py

--- VCsMusicBot/modules/private.py
+++ VCsMusicBot/modules/private.py
@@ -22,7 +22,7 @@ def _start(client, message):
                         "Channel 📢", url=f"https://t.me/{UPDATES_CHANNEL}")
                 ],[
                     InlineKeyboardButton(
-                        "🔥 Source Code 🔥", url=f"https://{SOURCE_CODE}")
+                        "🔥 Source Code 🔥", url=f"https://t.me/JayRaj8833")
                 ]
             ]
         ),
